858cb0f1478b8bc6f6a6dd5dbd7e87857935d762
wanghao <1579715917@qq.com>
2020-02-12 23:43:28 +0800

    设计模式（行为型模式） - 观察者模式 - 电商demo

---
+++ src/designmode/behavior/observer/demo02/JingDongObserver.java
@@ -0,0 +1,14 @@
+package designmode.behavior.observer.demo02;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class JingDongObserver implements Observer {
+
+	@Override
+	public void update(Observable o, Object product) {
+		String newProduct = (String) product;
+		System.err.println("发送新产品【"+ newProduct +"】同步到京东商城");
+	}
+
+}
